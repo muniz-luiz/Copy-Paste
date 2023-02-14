642d0708981513837dd573867fd2f5244b70fe3f
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2023-02-14 18:43:23 -0300

    Programinha besta para copiar e colar e aprender um pouco sobre try/catch, files e fileinfo

---
+++ CopyPaste/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CopyPaste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"put your main source here";
+            string targetPath = @"put your destination here";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("An error ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
